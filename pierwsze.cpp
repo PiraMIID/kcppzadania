9445051c080a565bb9b87b69cb6b9e4f342b0763
PiraMIID <55114984+PiraMIID@users.noreply.github.com>
2021-03-10 10:13:55 +0100

    1

---
+++ pierwsze.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dszma on 10.03.2021.
+//
+
+#include "pierwsze.h"
---
+++ pierwsze.h
@@ -0,0 +1,14 @@
+//
+// Created by dszma on 10.03.2021.
+//
+
+#ifndef ZADANIACPP_PIERWSZE_H
+#define ZADANIACPP_PIERWSZE_H
+
+
+class pierwsze {
+
+};
+
+
+#endif //ZADANIACPP_PIERWSZE_H
